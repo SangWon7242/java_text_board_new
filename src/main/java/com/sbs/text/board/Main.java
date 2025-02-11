2fb6eef14b84cff298b133c5ca9f63caf4ebe7d9
SangWon7242 <gotkdqja@gmail.com>
2025-02-11 11:21:36 +0900

    게시물 등록 기능 구현

--- src/main/java/com/sbs/text/board/Main.java
+++ src/main/java/com/sbs/text/board/Main.java
@@ -1,7 +1,36 @@
 package com.sbs.text.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("== 자바 텍스트 게시판 2025 ==");
+
+    while (true) {
+      System.out.print("명령) ");
+      String cmd = sc.nextLine();
+
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 작성 ==");
+
+        System.out.print("제목 : ");
+        String subject = sc.nextLine();
+
+        System.out.print("내용 : ");
+        String content = sc.nextLine();
+
+        int id = 1;
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      } else if (cmd.equals("exit")) {
+        break;
+      } else {
+        System.out.println("잘못 된 명령어입니다.");
+      }
+    }
+    System.out.println("== 게시판 종료 ==");
+
+    sc.close();
   }
 }
