73fea4e7516e98bae4772f262c4040621d7a6813
SangWon7242 <gotkdqja@gmail.com>
2025-02-11 11:34:27 +0900

    Article 생성자 도입

--- src/main/java/com/sbs/text/board/Main.java
+++ src/main/java/com/sbs/text/board/Main.java
@@ -24,11 +24,9 @@ public class Main {
         String content = sc.nextLine();
         int id = ++lastArticleId;
 
-        Article article = new Article();
-        article.id = id;
-        article.subject = subject;
-        article.content = content;
+        Article article = new Article(id, subject, content);
 
+        System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       } else if (cmd.equals("exit")) {
         break;
@@ -46,4 +44,10 @@ class Article {
   int id;
   String subject;
   String content;
+
+  public Article(int id, String subject, String content) {
+    this.id = id;
+    this.subject = subject;
+    this.content = content;
+  }
 }
