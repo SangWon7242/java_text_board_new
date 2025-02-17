d5a06b1cb8d9d016e9a0e985b621e29366961818
SangWon7242 <gotkdqja@gmail.com>
2025-02-17 12:23:26 +0900

    게시물 리스트를 역순으로 출력

--- src/main/java/com/sbs/text/board/Main.java
+++ src/main/java/com/sbs/text/board/Main.java
@@ -1,9 +1,13 @@
 package com.sbs.text.board;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    List<Article> articles = new ArrayList<>();
     int lastArticleId = 0;
 
     Scanner sc = new Scanner(System.in);
@@ -25,9 +29,23 @@ public class Main {
         int id = ++lastArticleId;
 
         Article article = new Article(id, subject, content);
+        articles.add(article);
 
         System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+      } else if (cmd.equals("/usr/article/list")) {
+        System.out.println("== 게시물 리스트 ==");
+
+        if(articles.isEmpty()) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        for(int i = articles.size() - 1; i >= 0; i--) {
+          Article article = articles.get(i);
+          System.out.printf("%d / %s\n", article.id, article.subject);
+        }
+
       } else if (cmd.equals("exit")) {
         break;
       } else {
